73f4b11af4bbc3717bf949dab35533943de50f41
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2023-09-07 16:55:16 +0300

    Created and emplemented 3 blog articles

--- src/scriptForBlog.js
+++
@@ -1,8 +0,0 @@
-// Scroling by ID
-
-window.onload = function () {
-  if (window.location.hash === "#scrollToArticle") {
-    const scrollToElement = document.getElementById("scrollToArticle");
-    scrollToElement.scrollIntoView({ behavior: "smooth" });
-  }
-};
---
+++ src/script_article-02.js
@@ -0,0 +1,13 @@
+// Open/Close Mobile Menu Logic
+
+const openButton = document.querySelector(".my__open-button");
+const closeButton = document.querySelector(".my__close-button");
+const mobileMenu = document.querySelector(".my__mobile-menu");
+
+openButton.addEventListener("click", function (e) {
+  mobileMenu.classList.remove("hidden");
+});
+
+closeButton.addEventListener("click", function (e) {
+  mobileMenu.classList.add("hidden");
+});
