6089add99d4f72bb057f23df9da86cd8d9822b73
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2024-09-17 16:13:40 +0300

    img fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'custom-margin': '24px', // Замініть '24px' на потрібне вам значення
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/aspect-ratio"),
