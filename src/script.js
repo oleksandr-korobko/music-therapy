269b927b3e621109c9df9a8f5cf16055e75656cf
Oleksandr Korobko <oleksandr.korobko@gmail.com>
2024-09-19 15:47:20 +0300

    Added JavaScript logic to hide the mobile menu when the .my__activity-directions is clicked

--- src/script.js
+++ src/script.js
@@ -3,12 +3,17 @@
 const openButton = document.querySelector('.my__open-button');
 const closeButton = document.querySelector('.my__close-button')
 const mobileMenu = document.querySelector('.my__mobile-menu');
+const activityDirections = document.querySelector('.my__activity-directions');
 
 openButton.addEventListener('click', function(e) {
   mobileMenu.classList.remove('hidden');
 });
 
 closeButton.addEventListener('click', function(e) {
+  mobileMenu.classList.add('hidden');
+});
+
+activityDirections.addEventListener('click', function(e){
   mobileMenu.classList.add('hidden');
 });
 
